22914662a10e5dbdb97995b1c5ff81276661013d
mariyagcv <mariya.gcv@gmail.com>
2018-09-16 12:45:07 +0300

    test parameters to server

--- zh.py
+++ zh.py
@@ -14,26 +14,8 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/submitForm', methods = ['POST'])
-def formInput():
-    start = request.form['start']
-    end = request.form['end']
-    # end = request.form['end'].encode('utf-8')
-
-
-# requesting ricetions and stuffz
-
-    gmaps = googlemaps.Client(key='AIzaSyCOcZOEW2Q07uQmpCDj4VCBanEW1yJerBM')
-
-    # # Geocoding an address
-    # geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-    #
-    # # Look up an address with reverse geocoding
-    # reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-    # Request directions via public transit
-    now = datetime.now()
-    directions_result = gmaps.directions(start,
-                                         end,
-                                         mode="transit",
-                                         departure_time=now)
+@app.route('/prediction', methods = ['GET'])
+def received():
+    time = request.args.get('arrival_time')
+    print (time)
+    return "5"
